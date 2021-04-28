8ff692278510aa90dff9e552f079d517b606b165
DenisKosachov <kosachovdenis@gmail.com>
2021-04-28 14:23:12 +0300

    End of Qa2homeworkOne

--- src/test/java/Qa2HomeworkOne.java
+++ src/test/java/Qa2HomeworkOne.java
@@ -1,4 +1,3 @@
-// import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,10 +8,22 @@ import org.openqa.selenium.WebElement;
 import java.util.List;
 
 
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class Qa2HomeworkOne {
 
     private final By ACCEPT_COOKIES_BTN = By.xpath(".//button[@mode = 'primary']");
 
+    /* Тест 1: Задание: найти первую статью и перейти в камментарии. Я использовал список, хотя можно
+    было использовать FindElement и он выбрал бы первый из найденых. Но если бы нам нужна была вторая статья,
+    то пришлось бы использовать список. Поэтому я сразу его использовал.
+
+    Когда я запускал тест, я наткнулся на статью где коменнтарии были отключены. Тест начал ругаться, я
+    использовал: assertTrue(commentButtons.size()==1, "Comments disabled"); но потом когда кнопка появилась,
+    тест на неё не нажимал. List <WebElement> commentButtons - использован, потому что есть и другие кнопки.
+    Вопрос: когда и как правильно использовать assertTrue?
+    */
     @Test
     public void homeworkOneTaskOne() {
 
@@ -28,11 +39,14 @@ public class Qa2HomeworkOne {
         wait.until(ExpectedConditions.elementToBeClickable(headlines.get(0)));
         headlines.get(0).click();
 
-        WebElement commentButton = browserWindow.findElement(By.xpath(".//img[@src = '/v5/img/icons/comment-v2.svg']"));
+        List <WebElement> commentButtons = browserWindow.findElements(By.xpath(".//img[@src = '/v5/img/icons/comment-v2.svg']"));
+//        assertTrue(commentButtons.size()==1, "Comments disabled");
+        WebElement commentButton = commentButtons.get(0);
         wait.until(ExpectedConditions.elementToBeClickable(commentButton));
         commentButton.click();
     }
 
+    // Тест 2: Задание: Вывести заголовок первой статьи.
     @Test
     public void homeworkOneTaskTwo() {
 
@@ -47,7 +61,7 @@ public class Qa2HomeworkOne {
         List <WebElement> headlines = browserWindow.findElements(By.xpath(".//span[@class = 'list-article__headline']"));
         System.out.println("1. " + headlines.get(0).getText());
     }
-
+    // Тест 3: Задание: Найти локаторы статей, коментариев, логотипа, кнопки перехода на русский TvNet.
     @Test
     public void homeworkOneTaskThree() {
 
@@ -68,11 +82,25 @@ public class Qa2HomeworkOne {
         List <WebElement> logo = browserWindow.findElements(By.xpath(".//img[@src = 'https://f.pmo.ee/logos/4133/7b1236dab95abca45083322781760e97.svg']"));
         System.out.println("Logo: " +  logo.size());
 
-//        List <WebElement> languаge = browserWindow.findElements(By.xpath(".//a[@href = 'https://rus.tvnet.lv/?_ga=2.139661619.161187411.1619360489-315548803.1619360489']"));
-//        System.out.println("Language: " +  languаge.size());
+        WebElement menuTop = browserWindow.findElement(By.xpath(".//div[@class = 'menu-items menu-items--top']"));
+        List <WebElement> listItemsMenuTop = menuTop.findElements(By.className("menu-item"));
+        for (WebElement itemMenuTop: listItemsMenuTop) {
+            if (itemMenuTop.getText().toLowerCase().contains("rus")) {
+                itemMenuTop.click();
+                System.out.println("Language change.");
+            }
+        }
     }
 
-    @Test
+    /* Тест 4: Задание: Вывести заголовоки всех статей без количества комментариев.
+    2 и 3 версии похожи. Есть вопрос: правильней создавать переменную String head = headlines.get(i).getText();
+    как во второй версии или лучше без неё. Оби версии работают не правильно, так как в статье могут быть "("
+    и тогда название статьи обрежится. Причём не важно с начала названия или с конца проверять.
+    Я попробывал найти под элемент в первом варианте, но не смог разобраться с:
+    if (!headlines.get(i).findElements(By.className("list-article__comment section-font-color")).isEmpty())
+    Вопрос: как проверить наличие элемента?
+     */
+/*    @Test
     public void homeworkOneTaskFourVerOne() {
 
         System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
@@ -83,19 +111,15 @@ public class Qa2HomeworkOne {
         wait.until(ExpectedConditions.presenceOfElementLocated(ACCEPT_COOKIES_BTN));
         browserWindow.findElement(ACCEPT_COOKIES_BTN).click();
 
-        List <WebElement> headlines = browserWindow.findElements(By.xpath(".//span[@class = 'list-article__headline']"));
-        for(int i=0; i < headlines.size(); i++)
-        {
-            int removeComment = headlines.get(i).getText().indexOf("(");
-            if (removeComment == -1) {
-                System.out.println(i+1 + ". " + headlines.get(i).getText());
-            }
-            else{
-                System.out.println(i+1 + ". " + headlines.get(i).getText().substring(0, removeComment));
+        List<WebElement> headlines = browserWindow.findElements(By.xpath(".//span[@class = 'list-article__headline']"));
+        for (int i = 0; i < headlines.size(); i++) {
+            if (!headlines.get(i).findElements(By.className("list-article__comment section-font-color")).isEmpty()) {
+                System.out.println(i + 1 + " Comment avaible");
             }
         }
     }
 
+*/
     @Test
     public void homeworkOneTaskFourVerTwo() {
 
@@ -145,6 +169,11 @@ public class Qa2HomeworkOne {
         }
     }
 
+    /* Тест 5: Задание: Вывести загаловки всех статей с количеством элементов.
+    По скольку я не смог запустить проверку:
+    if (!headlines.get(i).findElements(By.className("list-article__comment section-font-color")).isEmpty()) {
+    а поиск по символу "(" обрезает заголовки статей, я оставил такой вариант.
+     */
     @Test
     public void homeworkOneTaskFive() {
 
@@ -166,3 +195,8 @@ public class Qa2HomeworkOne {
     }
 
 }
+/*
+Итог: два вопроса.
+1. когда и как правильно использовать assertTrue?
+2. как проверить наличие элемента?
+ */
